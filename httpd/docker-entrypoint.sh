ce6d7121e8264c527be547272cf8aa2fdf33963f
Emanuele Disco <emawind84@gmail.com>
2017-02-12 01:49:31 +0900

    bug fix

--- httpd/docker-entrypoint.sh
+++ httpd/docker-entrypoint.sh
@@ -15,7 +15,7 @@ echo "- ssl chain file:  ${APACHE_SSL_CHAIN}"
 if [ "$APACHE_SSL" == "1" ]; then
     /usr/sbin/a2enconf ssl.conf
     sed -i 's/*:80/*:443/' /etc/apache2/sites-available/default.conf
-    sed -i 's/#SSLEngine On/SSLEngine On/' /etc/apache2/sites-available/default.conf
+    sed -i 's/#SSL/SSL/' /etc/apache2/sites-available/default.conf
 fi
 
 if [ $# -eq 0 ]; then
